78fdda1c3f024dfc233269c85577e038873e10ce
Kunal Jadhav <127647392+Kunal-here@users.noreply.github.com>
2024-01-10 15:56:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,42 +9,6 @@ let genBtn = document.getElementById("genBtn");
 let copyIcon = document.getElementById("copyIcon");
 
 
-// sliderValue.textContent = inputSlider.value;
-// inputSlider.addEventListener('input',()=>{
-//     sliderValue.textContent = inputSlider.value;
-// })
-
-// genBtn.addEventListener('click',()=>{
-//     passBox.value = generatePassword();
-// })
-
-// let lowerChars = "abcdefghijklmnopqrstuvwxyz";
-// let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// let allNumbers = "0123456789";
-// let allSymbols = "~!@#$%^&*"; 
-
-// function generatePassword(){
-//     let genPassword = "";
-//     let allChars = "";
-
-//     allChars += lowercase.checked ? lowercase : "";
-//     allChars += uppercase.checked ? upperChars : "";
-//     allChars += numbers.checked ? allNumbers : "";
-//     allChars += symbols.checked ? allSymbols : "";
-
-//     if(allChars == "" || allChars.length == 0){
-//         return genPassword;
-//     }
-
-//     let i = 0;
-//     while(i <= inputSlider.value){
-//         genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length))
-//         i++;
-//     }
-
-//     return genPassword;
-// }
-
 
 sliderValue.textContent = inputSlider.value;
 inputSlider.addEventListener('input',()=>{
